9994a9a6d32562ad6c21def5efffe65c3fb9b6c6
Hunter <hbingham@ucsc.edu>
2018-04-18 21:56:46 -0700

    test4 working, writing pageceount to file

--- codebase/rbf/pfm.cc
+++ codebase/rbf/pfm.cc
@@ -29,7 +29,10 @@ RC PagedFileManager::createFile(const string &fileName)
 {
     if(access(fileName.c_str(), F_OK ) == -1)
     {
-       FILE *fp = fopen(fileName.c_str(), "w");
+       unsigned int pageCount = 0;
+       FILE *fp = fopen(fileName.c_str(), "w+b");
+       fwrite(&pageCount, sizeof(unsigned), 1, fp);
+       fseek(fp, 0, SEEK_SET);
        fclose(fp);
        return 0;
     }
@@ -51,18 +54,21 @@ RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle)
 {
     if(access(fileName.c_str(), F_OK ) == 0)
     {
-       FILE *fp = fopen(fileName.c_str(), "rw");
+       FILE *fp = fopen(fileName.c_str(), "r+b");
        fileHandle.fd = fp;
        return 0;
     }
-    return -1;
+    return -1; //couldnt open file for some reason
 }
 
 
 RC PagedFileManager::closeFile(FileHandle &fileHandle)
 {
-    fclose(fileHandle.fd);
-    return 0;
+    if (fclose(fileHandle.fd) == 0)
+{
+        return 0;
+}
+    return -1;
 }
 
 
@@ -93,17 +99,40 @@ RC FileHandle::writePage(PageNum pageNum, const void *data)
 
 RC FileHandle::appendPage(const void *data)
 {
-    return -1;
+    fseek(fd, 0, SEEK_END);
+    fwrite(data, sizeof(char), PAGE_SIZE, fd);
+    incPageCount();
+    appendPageCounter++;
+    return 0;
 }
 
 
 unsigned FileHandle::getNumberOfPages()
 {
-    return 0;
+    fseek(fd, 0, SEEK_SET);
+    unsigned int pageNum;
+    fread(&pageNum, sizeof(unsigned), 1, fd);
+    return pageNum;
 }
 
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount)
 {
-	return -1;
+    readPageCount = readPageCounter;
+    writePageCount = writePageCounter;
+    appendPageCount = appendPageCounter;
+    return 0;
+}
+
+
+
+RC FileHandle::incPageCount()
+{
+    fseek(fd, 0, SEEK_SET);
+    unsigned int pageCount;
+    fread(&pageCount, sizeof(unsigned), 1, fd);
+    pageCount++;
+    fseek(fd, 0, SEEK_SET);
+    fwrite(&pageCount, sizeof(unsigned), 1, fd);
+    return 0;
 }
